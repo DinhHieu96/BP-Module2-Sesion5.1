c846ce2bcc0a3f5011922651848623829fe0056d
DinhHieu96 <truongdinhhieu1496@gamil.com>
2019-08-06 11:21:15 +0700

    thuc hanh

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import com.company.animal.Animal;
+import com.company.animal.Chicken;
+import com.company.animal.Tiger;
+import edible.Edible;
+import fruit.Apple;
+import fruit.Fruit;
+import fruit.Orange;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal[] animals=new Animal[2];
+        animals[0]=new Tiger();
+        animals[1]=new Chicken();
+        Fruit[] fruits=new Fruit[2];
+        fruits[0]=new Apple();
+        fruits[1]=new Orange();
+        for (Animal animal:animals
+             ) {
+            System.out.println(animal.makeSound());
+            if (animal instanceof Chicken) {
+                Edible edible = (Chicken) animal;
+                System.out.println(edible.howToEat());
+            }
+
+        }
+        for (Fruit fruit:fruits
+             ) {
+            System.out.println(fruit.howToEat());
+        }
+	// write your code here
+    }
+}
---
+++ src/com/company/animal/Tiger.java
@@ -0,0 +1,11 @@
+package com.company.animal;
+
+import edible.Edible;
+
+public class Tiger extends Animal {
+    @Override
+    public String makeSound() {
+        return "Tiger: roammmm ";
+    }
+
+}
